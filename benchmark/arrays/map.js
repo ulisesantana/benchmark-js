af5093c708eb591d9b6f4b118a125dc0f39cb506
Ulises Santana <ulisesantana@gmail.com>
2020-10-10 00:56:22 +0100

    add Array mapping benchmark

---
+++ benchmark/arrays/map.js
@@ -0,0 +1,43 @@
+const Benchmark = require("../../lib/Benchmark");
+
+function execForOf(array) {
+  return () => {
+    const result = [];
+    for (const x of array) {
+      result.push(Math.random() * x);
+    }
+    return result;
+  };
+}
+
+function execMap(array) {
+  return () => array.map((x) => Math.random() * x);
+}
+
+function execProceduralFor(array) {
+  return () => {
+    const result = [];
+    for (let i = 0, max = array.length; i < max; i++) {
+      result.push(Math.random() * array[i]);
+    }
+    return result;
+  };
+}
+
+function execSuite(numberOfElements) {
+  const suite = new Benchmark(
+    `Transforming arrays with ${numberOfElements} elements`
+  );
+
+  const bigArray = Array(numberOfElements).fill(10);
+
+  suite
+    .add("For of", execForOf(bigArray))
+    .add("Array.map", execMap(bigArray))
+    .add("Procedural for", execProceduralFor(bigArray))
+    .printResults();
+}
+
+execSuite(100000);
+execSuite(10000);
+execSuite(1000);
--- example.js
+++ example.js
@@ -6,15 +6,15 @@ suite.options({
 });
 
 suite
-  .add("Array.concat", function () {
+  .add("Array.concat", () => {
     const array = [1, 2, 3, 4, 5];
     return array.concat(6);
   })
-  .add("Spread operator", function () {
+  .add("Spread operator", () => {
     const array = [1, 2, 3, 4, 5];
     return [...array, 6];
   })
-  .add("Array.push", function () {
+  .add("Array.push", () => {
     const array = [1, 2, 3, 4, 5];
     return array.push(6);
   })
@@ -22,21 +22,3 @@ suite
   .printResults(true)
   .printFastestResult()
   .printSlowestResult();
-
-const suite2 = new Benchmark("Transform arrays");
-
-const bigArray = Array(100000).fill(10);
-const bigArray2 = Array(100000).fill(10);
-
-suite2
-  .add("For of", function () {
-    const result = [];
-    for (const x of bigArray) {
-      result.push(Math.random() * x);
-    }
-    return result;
-  })
-  .add("Array.map", function () {
-    return bigArray2.map((x) => Math.random() * x);
-  })
-  .printResults();
